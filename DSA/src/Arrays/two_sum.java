84e7633bd35b8186f39fe5c41b19cd97fe3f0292
ROHIT M R <rohitmahishi99@gmail.com>
2023-06-01 14:41:21 +0530

    added the bruteforce, optimal and better solution

--- DSA/src/Arrays/two_sum.java
+++ DSA/src/Arrays/two_sum.java
@@ -1,6 +1,15 @@
 package Arrays;
 
 public class two_sum {
+	//brute-force approach:
+	//running 2 for loops --> (n**2, 1) even if the array is not sorted works in N**2.
+	
+	//better
+	// if the array is not sorted , sort the array and use 2 pointers, (NlogN,1).
+	
+	//Optimal
+	//If the array is sorted, use 2 pointers, (N,1)
+	
 	public static boolean checksum(int arr[], int target)
 	{
 		int first_ptr=0;
